3bd6cbd00934175ec8639a12f65288bc2f29046d
Office iMac <vibethemes@gmail.com>
2015-06-05 10:22:00 +0530

    Important functions
    Signed-off-by: Office iMac <vibethemes@gmail.com>

--- includes/class.config.php
+++ includes/class.config.php
@@ -4,6 +4,7 @@ abstract class bpc_config{
 
 	var $version = '1.0';
 	var $option = 'bp_social_connect';
+	var $security_key = 'bp_social_connect_security';
 	var $social_options = array(
 		'facebook',
 		'google',
@@ -24,4 +25,49 @@ abstract class bpc_config{
 		update_option($this->option,$value);
 	}
 
+	function get_current_url(){
+    	global $post;
+		if ( is_front_page() ) :
+			$page_url = home_url();
+			else :
+			$page_url = 'http';
+		if ( isset( $_SERVER["HTTPS"] ) && $_SERVER["HTTPS"] == "on" )
+			$page_url .= "s";
+				$page_url .= "://";
+				if ( $_SERVER["SERVER_PORT"] != "80" )
+			$page_url .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+				else
+			$page_url .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+			endif;
+			
+		return esc_url( $page_url );
+	}
+	
+	function grab_avatar($link,$type,$user_id){
+		$base_dir=wp_upload_dir();
+		$dir = $base_dir['basedir'].'/avatars/'.$user_id;
+		is_dir($dir) || @mkdir($dir) || die(__("Can't Create folder","bp_social_connect"));
+		if($type=='thumb'){
+			copy($link, $dir .'/'.$user_id.'-bpthumb.jpg');	
+		}else if($type=='full'){
+			copy($link, $dir .'/'.$user_id.'-bpfull.jpg');
+		}
+	}
+	
+	function force_login( $user_email, $remember=true ) { 
+		ob_start();
+		if ( !is_user_logged_in() ) {
+			$user = get_user_by('email', $user_email );
+			wp_set_current_user( $user->ID, $user->user_login );
+			wp_set_auth_cookie( $user->ID, $remember );
+			do_action( 'wp_login', $user->user_login );
+		} else {
+			wp_logout();
+			$user = get_user_by('email', $user_email );
+			wp_set_current_user( $user->ID, $user->user_login );
+			wp_set_auth_cookie( $user->ID, $remember );
+			do_action( 'wp_login', $user->user_login );
+		}
+		ob_end_clean();
+	}
 }
