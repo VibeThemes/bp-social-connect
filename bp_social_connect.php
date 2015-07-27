409bc054a4dfe9bd9aa5d3a869713bfacf673075
Ripul Kumar <vibethemes@gmail.com>
2015-07-27 16:46:11 +0530

    version1.1
    Signed-off-by: Ripul Kumar <vibethemes@gmail.com>

--- bp_social_connect.php
+++ bp_social_connect.php
@@ -3,7 +3,7 @@
 Plugin Name: BuddyPress Social Connect
 Plugin URI: http://www.vibethemes.com
 Description: Connect BuddyPress Login and Registration with Social Networks
-Version: 1.0
+Version: 1.1
 Author: VibeThemes
 Author URI: http://www.VibeThemes.com/
 License : GPLv2 or Later
